64a8bbfbb24885856d846a1a96e336bafc105c81
LIMDAEYEONG <ldy0703@naver.com>
2017-05-22 16:16:02 +0900

    SpringSetting
    SpringSetting

---
+++ RestApplication.java
@@ -0,0 +1,16 @@
+package com.cnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dy on 2017-05-22.
+ */
+@SpringBootApplication
+public class RestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestApplication.class, args);
+    }
+
+
+}
